4ad91d527617d5645584cf7595416b9c5a49d8f4
nguyen robert <robert.nguyen@meritis.fr>
2016-06-30 15:44:00 +0200

    first commit test

---
+++ JenkinsTestBuild/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsTestBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test Jenkins ");
+            Console.ReadKey();
+        }
+    }
+}
