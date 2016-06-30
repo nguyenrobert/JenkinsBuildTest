e4b9ddb14e53612b538b6e16588849d013607502
nguyen robert <robert.nguyen@meritis.fr>
2016-06-30 15:52:23 +0200

    3rd commit test

---
+++ JenkinsTestBuild.Services/ServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsTestBuild.Services
+{
+    public class ServiceTest
+    {
+        public string test()
+        {
+            return "Test service";
+        }
+    }
+}
--- JenkinsTestBuild/Program.cs
+++ JenkinsTestBuild/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JenkinsTestBuild.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,11 @@ namespace JenkinsTestBuild
         {
             Console.WriteLine("test Jenkins ");
             Console.WriteLine("test Jenkins update ");
+            Console.WriteLine("test Jenkins update with dépendance '"+ (new ServiceTest().test()) +"' ");
             Console.ReadKey();
+
+
+
         }
     }
 }
